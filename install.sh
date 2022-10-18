a7fb9061107462106b433401e6829c22c819fabf
JC TINIO <s20016@std.it-college.ac.jp>
2022-10-18 12:04:40 +0900

    Fix: Typo

--- install.sh
+++ install.sh
@@ -25,17 +25,16 @@ fi
 
 # Installing NVIM Config
 if [ ! -d $CONFIG_DIR ]; then
-  sudo apt install -y neovim
-  sudo apt install curl
+  sudo apt install -y curl git neovim
   mkdir -p $CONFIG_DIR
   sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
   cd $TMP_DIR && git clone https://github.com/s20016/MV-Frame.git
 	cp $TMP_DIR/MV-Frame/conf/init.vim $CONFIG_DIR/.
-  nvim +'PlugInstall --sync' +qa
 	cp $TMP_DIR/MV-Frame/conf/setting.vim $CONFIG_DIR/.
 	cp $TMP_DIR/MV-Frame/conf/mapping.vim $CONFIG_DIR/.
-	cp $TMP_DIR/MV-FrameMugenVim/conf/plugins.vim $CONFIG_DIR/.
+	cp $TMP_DIR/MV-Frame/conf/plugins.vim $CONFIG_DIR/.
+  nvim +'PlugInstall --sync' +qa
   mkdir $SESSIO_DIR
   nvim +'source $CONFIG_DIR/init.vim' +q
 fi
